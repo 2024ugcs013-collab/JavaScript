681a615695594a3e0d6343348261f91b5223ca31
2024ugcs013-collab <2024ugcs013@nitjsr.ac.in>
2025-12-10 07:33:55 +0000

    config3 dn

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict" ;// use all js code as newer version
+/*
+number -> 2 to powr 53
+string -> " "
+bigint 
+null->stand alone value
+undefined
+symbol->unique
+boolean
+*/
+// object
+console.log(typeof undefined) // undefined
+console.log(typeof null)//object
